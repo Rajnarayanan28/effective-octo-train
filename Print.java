85557a6635975ae059cbbc460b51fe3680ec4042
Raj <Rajnarayanan666@gmail.com>
2025-05-12 16:16:32 +0400

    this is making the user enter values to file

--- Print.java
+++ Print.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 class Print{
     public static void main(String args[]){
         System.out.println("Printing contents");
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        System.out.println("well well the digit is : " + a);
     }
 }
